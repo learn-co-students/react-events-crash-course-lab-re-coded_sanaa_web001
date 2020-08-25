94f17c913a090ef2ffd6e221c1551a145c906d69
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-08-25 23:06:03 +0300

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,28 +10,49 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    drawChromeBoiAtCoords( event.clientX,event.clientY)
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+  //  toggleCycling=()=>{
+
+  //  }
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+  handleKeyDown = (event) => {
+    if (event.key === 'a') {
+      resize('+')
+    } else if (event.key === 's') {
+      resize('-')
+    }
+  }
+   
   
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}}
+        onKeyDown={this.handleKeyDown}
         width='900'
         height='600'
-        tabIndex="0">
+        tabIndex="0"
+        >
       </canvas>
     )
   }
 }
+
+// <ChromeBoisDomain />
+// 1) invokes the `drawChromeBoiAtCoords` method within `handleMouseMove`, passing the captured x and y values of the mouse from the event
+// 2) has an event listener for clicks on the <canvas> element that triggers `toggleCycling`
+// 3) has an event listener for key presses on the <canvas> element that triggers `resize`
+// 4) when the 'a' key is pressed, `resize` is invoked with the argument of '+'
+// 5) when the 's' key is pressed, `resize` is invoked with the argument of '-'
